c0905a2e192bf9f399d15ba22c816854c768a8d7
Russell Jones <jonesrussell42@gmail.com>
2017-07-05 19:03:23 -0400

    Added facebook signin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,16 +2,37 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+	displayName;
 	items: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController, afDB: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, public afDB: AngularFireDatabase, private afAuth: AngularFireAuth) {
+  	afAuth.authState.subscribe(user => {
+ 	if (!user) {
+ 		this.displayName = null;        
+		return;
+	}
+ 		this.displayName = user.displayName;      
+	});
 	this.items = afDB.list('/cuisines');
   }
+
+  signInWithFacebook() {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then(res => console.log(res));
+  }
+
+  signOut() {
+		this.afAuth.auth.signOut();
+	}
 
 }
