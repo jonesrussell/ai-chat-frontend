0c178d519a8c3e5a2f6adeb47925a4b4dae7ac57
Russell Jones <jonesrussell42@gmail.com>
2018-02-22 20:25:20 -0500

    Updated env vars

--- environment-variables/development.ts
+++ environment-variables/development.ts
@@ -1,8 +1,8 @@
 export const devVariables = {
 	environmentName: 'Development Environment',
 	ionicEnvName: 'dev',
-//	aiEndpoint: 'http://192.168.2.238:3000',
-	aiEndpoint: 'https://ai-chat-legion.herokuapp.com',
+	//aiEndpoint: 'http://localhost:3000',
+	aiEndpoint: 'http://ai-chat-legion.herokuapp.com',
 	messagePath: '/messages',
 	token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOm51bGwsImV4cCI6Mjc5Mzk4NDQ3MCwiYXVkIjoiIiwic3ViIjoiIn0.2EHxpSNjZum9DIchthLARjOhlfCz0HTR9IJGJ7z5340'
 };
