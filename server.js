78db7c8e8c259c1d4964b7557dbec8e660fcb74a
Russell Jones <jonesrussell42@gmail.com>
2018-02-24 03:14:58 -0500

    cors testing

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-var express = require('express'),
-    app = express();
+let express = require('express'),
+    app = express(),
+	cors = require('cors');
 
+app.use(cors());
 app.use(express.static('www'));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.all('*', function(req, res, next) {
+/*app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
-});
+});*/
 
 // API Routes
 // app.get('/blah', routeHandler);
