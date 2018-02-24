f8601167fbfbfaa467af4f8abb2d227193b8804a
Russell Jones <jonesrussell42@gmail.com>
2018-02-24 02:33:35 -0500

    https

--- environment-variables/development.ts
+++ environment-variables/development.ts
@@ -2,7 +2,7 @@ export const devVariables = {
 	environmentName: 'Development Environment',
 	ionicEnvName: 'dev',
 	//aiEndpoint: 'http://localhost:3000',
-	aiEndpoint: 'http://ai-chat-legion.herokuapp.com',
+	aiEndpoint: 'https://ai-chat-legion.herokuapp.com',
 	messagePath: '/messages',
 	token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOm51bGwsImV4cCI6Mjc5Mzk4NDQ3MCwiYXVkIjoiIiwic3ViIjoiIn0.2EHxpSNjZum9DIchthLARjOhlfCz0HTR9IJGJ7z5340'
 };
--- environment-variables/production.ts
+++ environment-variables/production.ts
@@ -2,7 +2,7 @@ export const prodVariables = {
 	environmentName: 'Production Environment',
 	ionicEnvName: 'prod',
 	//aiEndpoint: 'http://192.168.2.238:3000',
-	aiEndpoint: 'http://ai-chat-legion.herokuapp.com',
+	aiEndpoint: 'https://ai-chat-legion.herokuapp.com',
 	messagePath: '/messages',
 	token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOm51bGwsImV4cCI6Mjc5Mzk4NDQ3MCwiYXVkIjoiIiwic3ViIjoiIn0.2EHxpSNjZum9DIchthLARjOhlfCz0HTR9IJGJ7z5340'
 };
